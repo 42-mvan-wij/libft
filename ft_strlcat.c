0515729ecd583a81f92f7fb49dd080d3aef6f8a9
Mariusmivw <mariusmivw@gmail.com>
2020-11-03 15:03:38 +0100

    updated so moulinette won't complain

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mvan-wij <mvan-wij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 18:24:08 by mvan-wij      #+#    #+#                 */
-/*   Updated: 2020/11/03 14:48:51 by mvan-wij      ########   odam.nl         */
+/*   Updated: 2020/11/03 14:58:33 by mvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,10 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	unsigned int init_dstsize;
-	unsigned int srcsize;
-	unsigned int i;
+	size_t init_dstsize;
 
-	srcsize = ft_strlen(src);
 	init_dstsize = ft_strlen(dst);
 	if (init_dstsize > dstsize)
-		return (dstsize + srcsize);
-	i = 0;
-	while (i < srcsize && i + init_dstsize < dstsize - 1)
-	{
-		dst[init_dstsize + i] = src[i];
-		i++;
-	}
-	if (dstsize > init_dstsize)
-		dst[init_dstsize + i] = '\0';
-	return (init_dstsize + srcsize);
+		return (dstsize + ft_strlen(src));
+	return (init_dstsize + ft_strlcpy(dst + init_dstsize, src, dstsize));
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mvan-wij <mvan-wij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 18:24:08 by mvan-wij      #+#    #+#                 */
-/*   Updated: 2020/11/03 14:48:50 by mvan-wij      ########   odam.nl         */
+/*   Updated: 2020/11/03 15:02:31 by mvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,16 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	unsigned int src_len;
+	unsigned int srcsize;
 	unsigned int i;
 
-	if (src == NULL)
-		return (0);
-	src_len = ft_strlen(src);
-	if (dst == NULL || dstsize == 0)
-		return (src_len);
-	i = 0;
-	while (i < src_len && i < dstsize - 1)
+	srcsize = ft_strlen(src);
+	if (srcsize < dstsize - 1)
+		ft_memcpy(dst, src, (srcsize + 1) * sizeof(char));
+	else if (dstsize != 0)
 	{
-		dst[i] = src[i];
-		i++;
+		ft_memcpy(dst, src, (dstsize - 1) * sizeof(char));
+		dst[dstsize - 1] = '\0';
 	}
-	dst[i] = '\0';
-	return (src_len);
+	return (srcsize);
 }
