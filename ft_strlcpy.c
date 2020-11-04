cf2594d8e6f76df3ab63f6d2b9db4f5e07fa4c2b
Mariusmivw <mariusmivw@gmail.com>
2020-11-04 14:04:45 +0100

    y2

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mvan-wij <mvan-wij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 18:24:08 by mvan-wij      #+#    #+#                 */
-/*   Updated: 2020/11/03 16:01:24 by mvan-wij      ########   odam.nl         */
+/*   Updated: 2020/11/04 14:03:52 by mvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	srcsize;
 
 	srcsize = ft_strlen(src);
-	if (srcsize < dstsize - 1)
+	if (srcsize + 1 < dstsize)
 		ft_memcpy(dst, src, (srcsize + 1) * sizeof(char));
 	else if (dstsize != 0)
 	{
