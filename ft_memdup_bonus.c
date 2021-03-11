e0e3f09753c0cf2f4d58e4620b50e519711ebd2a
Marius van Wijk <mvan-wij@student.codam.nl>
2021-03-11 23:47:51 +0100

    added malloc protection

--- ft_memdup_bonus.c
+++ ft_memdup_bonus.c
@@ -6,23 +6,19 @@
 /*   By: mvan-wij <mvan-wij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/25 12:51:32 by mvan-wij      #+#    #+#                 */
-/*   Updated: 2021/03/07 23:56:07 by mvan-wij      ########   odam.nl         */
+/*   Updated: 2021/03/11 23:32:24 by mvan-wij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdlib.h>
 
 void	*ft_memdup(const void *src, size_t n)
 {
-	size_t	i;
 	void	*dst;
 
 	dst = malloc(n);
-	i = 0;
-	while (i < n)
-	{
-		((char *)dst)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dst);
+	if (dst == NULL)
+		return (NULL);
+	return (ft_memmove(dst, src, n));
 }
